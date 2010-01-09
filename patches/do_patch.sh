877fc7f5c477a8ebfd7c75705eb7c03ff3631a48
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2010-01-09 08:44:19 +0000

    ulinux: Add another out-of tree patch.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -6,5 +6,5 @@ patch $OPT ../libv4l/v4l2-apps/libv4l/libv4lconvert/control/libv4lcontrol.c libv
 patch $OPT ../libv4l/linux/drivers/media/video/uvc/uvc_status.c uvc_status.c.diff
 patch $OPT ../libv4l/linux/drivers/media/video/uvc/uvc_video.c uvc_video.c.diff
 patch $OPT ../libv4l/linux/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
-
+patch $OPT ../libv4l/linux/drivers/media/video/stk-webcam.h stk-webcam.h.diff
 
