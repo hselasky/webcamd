c61f6c4d2a121a7b2121e0f14548ea669a987a94
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2010-01-11 20:06:39 +0000

    ulinux: usbcam: Remove quirk.

--- kernel/linux_timer.c
+++ kernel/linux_timer.c
@@ -79,13 +79,6 @@ timer_exec(void *arg)
 
 	while (1) {
 
-		/*
-		 * XXX temporary workaround. Not sure if this is a
-		 * pthreads atomic problem or code problem. Make sure
-		 * no wait statements are stuck forever.
-		 */
-		wake_up_all(NULL);
-
 		atomic_lock();
 		jiffies64 += 8;		/* ms */
 restart:
