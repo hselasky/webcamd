52565cbfbb55aea0bb5462b6fb398630c4a86ef6
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2010-01-11 14:25:13 +0000

    ulinux: Add minor patch for gspca driver.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -8,4 +8,5 @@ patch $OPT ../libv4l/linux/drivers/media/video/uvc/uvc_video.c uvc_video.c.diff
 patch $OPT ../libv4l/linux/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
 patch $OPT ../libv4l/linux/drivers/media/video/stk-webcam.h stk-webcam.h.diff
 patch $OPT ../libv4l/v4l2-apps/libv4l/libv4lconvert/libv4lsyscall-priv.h libv4lsyscall-priv.h.diff
+patch $OPT ../libv4l/linux/drivers/media/video/gspca/gspca.c gspca.c.diff
 
