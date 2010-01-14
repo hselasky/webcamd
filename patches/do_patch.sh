b468a16f370d5f4b6fa135e638b63a2245bb7794
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2010-01-14 22:57:35 +0000

    Update patches.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -2,17 +2,13 @@
 
 OPT=-N
 
-# drivers
+#
+# driver patches - mostly to improve usability
+#
 
-patch $OPT ../v4l-dvb/v4l2-apps/libv4l/libv4lconvert/control/libv4lcontrol.c libv4lcontrol.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/uvc/uvc_status.c uvc_status.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/uvc/uvc_video.c uvc_video.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/stk-webcam.h stk-webcam.h.diff
-patch $OPT ../v4l-dvb/v4l2-apps/libv4l/libv4lconvert/libv4lsyscall-priv.h libv4lsyscall-priv.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/gspca/gspca.c gspca.c.diff
 
-# libv4l
-
-patch $OPT ../libv4l/v4l2-apps/libv4l/libv4lconvert/control/libv4lcontrol.c libv4lcontrol.c.diff
-patch $OPT ../libv4l/v4l2-apps/libv4l/libv4lconvert/libv4lsyscall-priv.h libv4lsyscall-priv.h.diff
