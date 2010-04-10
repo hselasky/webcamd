26748667cd197c9a4724bc948025aed52c1b2768
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2010-04-10 21:12:52 +0000

    ulinux+webcamd: Add patch for PCTV453E driver.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -10,5 +10,6 @@ patch $OPT ../v4l-dvb/linux/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/stk-webcam.h stk-webcam.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/gspca/gspca.c gspca.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/as102/as102_drv.h as102_drv.h.diff
+patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/pctv452e.c pctv452e.c.diff
 
 exit 0
