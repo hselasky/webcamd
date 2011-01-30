4115a9f67bab6ac10ea74f07d5b9316268589bb2
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-01-30 09:26:19 +0000

    webcamd+ulinux: Import patches from Juergen Lock.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -13,5 +13,8 @@ patch $OPT ../v4l-dvb/linux/drivers/media/dvb/as102/as102_drv.h as102_drv.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/pctv452e.c pctv452e.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dib0700_devices.c dib0700_devices.c.diff
 patch $OPT ../v4l-dvb/linux/include/media/videobuf-dvb.h videobuf-dvb.h.diff
+patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/af9015.c af9015.c.diff
+patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/pctv452e.c pctv452e.c.diff
+patch $OPT ../v4l-dvb/linux/include/linux/dvb/frontend.h frontend.h.diff
 
 exit 0
