7cd6d7ca2f6ddfd5d0672f4b1f256063491d37d0
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-03-05 18:22:02 +0000

    ulinux+webcamd: Remove nonexistant patch.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -10,7 +10,6 @@ patch $OPT ../v4l-dvb/linux/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/stk-webcam.h stk-webcam.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/video/gspca/gspca.c gspca.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/as102/as102_drv.h as102_drv.h.diff
-patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/pctv452e.c pctv452e.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dib0700_devices.c dib0700_devices.c.diff
 patch $OPT ../v4l-dvb/linux/include/media/videobuf-dvb.h videobuf-dvb.h.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/af9015.c af9015.c.diff
