3b7c9fefd542f9a1aeb8697b0afec48cdee4bacb
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-03-20 13:36:17 +0000

    ulinux+webcamd: Stability fixes for S2-3600.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -18,6 +18,8 @@ patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dw2102.c dw2102.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/stb0899_algo.c stb0899_algo.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/ttusb-budget/dvb-ttusb-budget.c dvb-ttusb-budget.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/usb-urb.c usb-urb.c.diff
+patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dvb-usb-urb.c dvb-usb-urb.c.diff
+patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/stb0899_drv.c stb0899_drv.c.diff
 cat lirc-patches.diff | patch $OPT
 cat af9013.c.diff | patch $OPT
 
