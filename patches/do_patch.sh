a10420b09100ef207dbfbfe9146d6a8b9402ce05
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-03-21 20:06:32 +0000

    ulinux+webcamd: Remove patch. This is a kernel problem.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -18,7 +18,6 @@ patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dw2102.c dw2102.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/stb0899_algo.c stb0899_algo.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/ttusb-budget/dvb-ttusb-budget.c dvb-ttusb-budget.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/usb-urb.c usb-urb.c.diff
-patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dvb-usb-urb.c dvb-usb-urb.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/stb0899_drv.c stb0899_drv.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/cx24116.c cx24116.c.diff
 cat lirc-patches.diff | patch $OPT
