a0b119dddca49e340f62a93b10656ad1cf5dbc3e
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-03-21 12:00:47 +0000

    ulinux+webamd: Revert last change.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -21,7 +21,6 @@ patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/usb-urb.c usb-urb.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/dvb-usb/dvb-usb-urb.c dvb-usb-urb.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/stb0899_drv.c stb0899_drv.c.diff
 patch $OPT ../v4l-dvb/linux/drivers/media/dvb/frontends/cx24116.c cx24116.c.diff
-patch $OPT ../v4l-dvb/linux/drivers/media/video/v4l2-dev.c v4l2-dev.c.diff
 cat lirc-patches.diff | patch $OPT
 cat af9013.c.diff | patch $OPT
 
