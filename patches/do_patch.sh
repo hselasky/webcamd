b25c35f5fdc6a86e399a1dbd8fe365c475963555
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-05-23 21:30:08 +0000

    ulinux+webcamd: Cleanup patches preparing for next release.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -8,24 +8,13 @@ OPT='-Nslf'
 
 patch $OPT ../media_tree/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
 patch $OPT ../media_tree/drivers/media/video/stk-webcam.h stk-webcam.h.diff
-patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/dib0700_devices.c dib0700_devices.c.diff
-patch $OPT ../media_tree/include/media/videobuf-dvb.h videobuf-dvb.h.diff
-patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/af9015.c af9015.c.diff
-patch $OPT ../media_tree/include/linux/dvb/frontend.h frontend.h.diff
-patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/dw2102.c dw2102.c.diff
-patch $OPT ../media_tree/drivers/media/dvb/frontends/stb0899_algo.c stb0899_algo.c.diff
-patch $OPT ../media_tree/drivers/media/dvb/ttusb-budget/dvb-ttusb-budget.c dvb-ttusb-budget.c.diff
 patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/usb-urb.c usb-urb.c.diff
-patch $OPT ../media_tree/drivers/media/dvb/frontends/stb0899_drv.c stb0899_drv.c.diff
-patch $OPT ../media_tree/drivers/media/dvb/frontends/cx24116.c cx24116.c.diff
+patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/af9015.c af9015.c.diff
+patch $OPT ../media_tree/drivers/media/video/gspca/gspca.c gspca.diff 
 
-#cat lirc-patches.diff | patch $OPT
-#cat af9013.c.diff | patch $OPT
-
+#
+# New driver
+#
 cat as102.diff | patch $OPT -p1
-cat dvb_net.diff | patch $OPT -p1
-cat gspca.diff | patch $OPT -p1
-cat tda7432.diff | patch $OPT -p1
-cat tda8261.diff | patch $OPT -p1
 
 exit 0
