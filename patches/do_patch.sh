fa1ecd2cc920903e68de243750dc7e512ef4873c
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2012-01-05 17:26:22 +0000

    ulinux+webcamd: Update patch-set for Linux Kernel V3.2.

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -6,14 +6,20 @@ OPT='-Nslf'
 # driver patches - mostly to improve usability
 #
 
-patch $OPT ../media_tree/drivers/media/video/stk-webcam.h stk-webcam.h.diff
-patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/usb-urb.c usb-urb.c.diff
+patch $OPT -R ../media_tree/drivers/media/dvb/dvb-core/dvb_frontend.c dvb_frontend.c.diff
+patch $OPT -R ../media_tree/drivers/media/video/uvc/uvc_video.c uvc_video.c.diff
+patch $OPT -R ../media_tree/drivers/media/video/uvc/uvcvideo.h uvcvideo.h.diff
+patch $OPT -R ../media_tree/drivers/media/video/v4l2-dev.c v4l2-dev.c.diff
+patch $OPT -R ../media_tree/include/linux/dvb/frontend.h frontend.h.diff
+patch $OPT -R ../media_tree/include/linux/input.h input.h.diff
 patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/af9015.c af9015.c.diff
+patch $OPT ../media_tree/drivers/media/dvb/dvb-usb/usb-urb.c usb-urb.c.diff
 patch $OPT ../media_tree/drivers/media/video/gspca/gspca.c gspca.diff 
+patch $OPT ../media_tree/drivers/media/video/stk-webcam.h stk-webcam.h.diff
 
-#
-# New driver
-#
-cat as102.diff | patch $OPT -p1
+sed -e "s/err[(]/cx_&/g" -e "s/info[(]/cx_&/g" -i .orig ../media_tree/drivers/media/dvb/frontends/cx24123.c
+sed -e "s/err[(]/dib_&/g" -e "s/info[(]/dib_&/g" -e "s/deb_dib_/deb_/g" -i .orig ../media_tree/drivers/media/dvb/frontends/dib3000mb.c
+sed -e "s/err[(]/dib_&/g" -e "s/info[(]/dib_&/g" -e "s/deb_dib_/deb_/g" -i .orig ../media_tree/drivers/media/dvb/frontends/dib3000mb_priv.h
+sed -e "s/sleep[(,]/do_&/g" -e "s/mdo_sleep/msleep/g" -i .orig ../media_tree/drivers/media/dvb/frontends/tda18271c2dd.c
 
 exit 0
