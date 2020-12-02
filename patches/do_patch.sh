aba33c26cbe3b5b75792e9fd14e962674f29dd37
Rozhuk Ivan <rozhuk.im@gmail.com>
2020-12-02 16:59:12 +0300

    ulinux+webcamd: add support Geniatech T220A, usbtv, STK1160, TM6000 based devices
    - Geniatech T220A / NXP TDA18273 silicon tuner (from https://github.com/ljalves/linux_media )
    - TM6000 based DVB-T & Analog TV USB Stick
    - STK1160 based analog video capture
    - usbtv dongles

--- patches/do_patch.sh
+++ patches/do_patch.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 set -e
-echo "Applying Linux kernel patches ..."
-cat 0001-Linux-kernel-patches-for-webcamd.patch | patch -Nslf -p1 -d ../media_tree
-exit 0
+THIS_SCRIPT_NAME=`readlink -nf ${0}`
+THIS_SCRIPT_DIR=`dirname ${THIS_SCRIPT_NAME}`
 
+for _FILE in `find "${THIS_SCRIPT_DIR}/" -maxdepth 1 -type f -name '*.patch'`; do
+	echo "Applying Linux kernel patch: ${_FILE}"
+	patch -Nslf -p1 --directory "${THIS_SCRIPT_DIR}/../media_tree" --input "${_FILE}"
+done
+
+exit 0
